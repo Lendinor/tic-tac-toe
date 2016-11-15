7089a2110ee42982139333b73d024b5dc7b3f3ae
Alexander Flores <xandar@dosflores.net>
2016-11-15 14:57:24 -0800

    Day2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,14 +3,38 @@
 var namespace = "http://www.w3.org/2000/svg"
 var canvas = getElementById("game-board")
 
+var currentPlayer = 1
+
+var ulclicked = 0
+var umclicked = 0
+var urclicked = 0
+var mlclicked = 0
+var mclicked = 0
+var mrclicked = 0
+var blclicked = 0
+var bmclicked = 0
+var brclicked = 0
+
+
+
 function ul()
 {
-var circle = document.createElementNS(namespace, "circle")
- circle.setAttribute("cx", 50)
- circle.setAttribute("cy", 50)
- circle.setAttribute("r", 12.5)
- circle.setAttribute("fill", "red")
- canvas.appendChild(circle)
+
+if(ulclicked == 0)
+  {
+  if(currentPlayer == 1)
+    {
+    var circle = document.createElementNS(namespace, "circle")
+      circle.setAttribute("cx", 50)
+      circle.setAttribute("cy", 50)
+      circle.setAttribute("r", 12.5)
+      circle.setAttribute("fill", "red")
+      canvas.appendChild(circle)
+
+      ulclicked = 1
+      currentPlayer = 2
+    }
+ }
 }
 function um()
 {
